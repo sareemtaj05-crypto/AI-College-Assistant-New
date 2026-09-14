49b202fedccd28ddc9bd592caf6e70672bb34c04
Sarim Taj <sareemtaj05@gmail.com>
2026-09-14 20:28:48 +0530

    Prepare Django for Render

--- config/settings.py
+++ config/settings.py
@@ -15,13 +15,21 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
+
 ALLOWED_HOSTS = [
     host.strip()
     for host in os.environ.get(
         "ALLOWED_HOSTS",
-        "127.0.0.1,localhost"
+        "127.0.0.1,localhost,.onrender.com"
     ).split(",")
     if host.strip()
+]
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+    "https://*.onrender.com",
 ]
 
 
